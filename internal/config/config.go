7da1a29b0096e5d51aec3b03ae9bf856b95db770
anton <jappinen.anton@protonmail.com>
2025-09-07 20:53:24 +0200

    added the config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,24 +1,65 @@
 package config
-import "os"
+
+import (
+	"encoding/json"
+	"os"
+)
 
 type Config struct {
+	DB_URL           string `json:"db_url"`
+	Current_Username string `json:"current_username"`
 }
 
 const configFileName string = ".gatorconfig.json"
 
-func getConfigFilePath() string, error {
-	filePath := ""
+func getConfigFilePath() (string, error) {
 
-	os.UserHomeDir
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	filePath := home + "/" + configFileName
 
 	return filePath, nil
 }
 
-func (c *Config) SetUsername() {
-
+func (c *Config) SetUsername(username string) {
+	c.Current_Username = username
 }
 
-func ReadJson() Config {
+func (c *Config) Write(cfg Config) error {
 
-	return Config{}
+	filepath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filepath, data, os.FileMode(0644))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Read() (Config, error) {
+
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var c = Config{}
+	if err := json.Unmarshal(data, &c); err != nil {
+		return Config{}, err
+	}
+
+	return c, nil
 }
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
-package gator
+package main
+
+import (
+	"fmt"
+	"gituhub.com/anton-jj/gator/internal/config"
+)
 
 func main() {
+	gatorConf, err := config.Read()
+	if err != nil {
+		return
+	}
+	gatorConf.SetUsername("anton")
+	gatorConf.Write(gatorConf)
+	fmt.Printf("%s\n%s\n", gatorConf.Current_Username, gatorConf.DB_URL)
 
 }
